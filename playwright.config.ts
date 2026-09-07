5845096a8da1bd5fabac6f241766d08f24c53fbe
YTanishka <tanishkayadav6000@gmail.com>
2026-09-07 18:21:05 +0530

    added-browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,26 +3,38 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
 
-   timeout: 60000,
+  timeout: 60000,
 
   reporter: [
     ["list"],
-    ["html", { outputFolder: "playwright-report", open: "never" }]
+    ["html", { outputFolder: "playwright-report", open: "never" }],
   ],
 
   use: {
     baseURL: "https://automationexercise.com",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    trace: "on-first-retry"
+    trace: "on-first-retry",
   },
 
   projects: [
     {
       name: "chromium",
       use: {
-        ...devices["Desktop Chrome"]
-      }
-    }
-  ]
+        ...devices["Desktop Chrome"],
+      },
+    },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+      },
+    },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+      },
+    },
+  ],
 });
